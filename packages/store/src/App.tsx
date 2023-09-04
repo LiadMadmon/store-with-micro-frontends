9e57792b9e11014bbacea7a19a14ba6d6139d792
liad.madmon <liad.madmon@vesttoo.com>
2023-09-04 15:13:28 +0300

    Add a working microfrontend infra

--- packages/store/src/App.tsx
+++ packages/store/src/App.tsx
@@ -1,54 +1,16 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { lazy, Suspense } from 'react'
 import './App.css'
+const Cart = lazy(() => import('cart/Cart'));
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    const apiUrl = `${import.meta.env.VITE_API_URL}api/v1/todos`;
-
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Response data:', data);
-      })
-      .catch((error) => {
-        // Handle any errors that occurred during the fetch
-        console.error('Fetch error:', error);
-      });
-  }, [])
-
+function Store() {
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Host Application</h1>
+        <Suspense fallback={<div>Loading Remote Component...</div>}>
+          <Cart />
+        </Suspense>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+export default Store
